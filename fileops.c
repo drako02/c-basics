183cd0da46ecf9b4b8d41ff82132b0b9a9fe603f
drako02 <andrewappah.aa@gmail.com>
2025-09-01 21:09:59 +0000

    Refactor read_file_content to handle file size and memory allocation correctly

--- fileops.c
+++ fileops.c
@@ -21,4 +21,26 @@ int read_file_content(const char *filename, char **content, size_t *size)
         
         return FILE_ERROR_NOT_FOUND;
     }
+
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    if (file_size < 0){
+        fclose(fp);
+        return FILE_ERROR_NOT_FOUND;
+    }
+
+    *content = malloc(file_size + 1);
+    if (content == NULL){
+        fclose(fp);
+        return FILE_ERROR_MEMORY;
+    }
+
+    size_t bytes_read = fread(*content, 1, file_size, fp);
+    (*content)[bytes_read] = '\0';
+
+    *size = bytes_read;
+    fclose(fp);
+    return FILE_SUCCESS;
 }
