b5b64e338b123dfaa25d596c16be9c3e81a804be
drako02 <andrewappah.aa@gmail.com>
2025-11-07 00:18:53 +0000

    Refactor print_permissions and implement print_detailed_file_info to enhance file information display

--- directories.c
+++ directories.c
@@ -40,4 +40,23 @@ int get_detailed_info(const char *path, detailed_file_info_t *info){
 
 void print_permissions(mode_t mode) {
     printf(S_ISDIR(mode) ? "d" : "-");
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w": "-");
+    printf((mode & S_IXUSR) ? "x": "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w": "-");
+    printf((mode & S_IXGRP) ? "x": "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w": "-");
+    printf((mode & S_IXOTH) ? "x": "-");
+
+}
+
+void print_detailed_file_info(const detailed_file_info_t *info ){
+    print_permissions(info->mode);
+
+    struct passwd *pw = getpwuid(info->uid);
+    struct group *gr = getgrgid(info->gid);
+
+    printf(" %8s %8s", pw ? pw->pw_name : "Unknown", gr ? gr->gr_name : "Unknown");
 }
